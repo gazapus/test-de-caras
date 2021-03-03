b9fbd53267a06db4f3fb29bba9b8fbd8bddd2f7e
Cristian Villafane <cristianv129@hotmail.com>
2021-03-03 03:22:59 -0300

    Componente CopyableText creado

---
+++ src/components/CopyableText.jsx
@@ -0,0 +1,100 @@
+import styled from 'styled-components';
+import brakpoints from '../utils/breakpoins';
+import { useRef, useState } from 'react';
+import Alert from './Alert';
+
+const StyledContainer = styled.div`
+    display: flex;
+    padding: 0.3em;
+    box-sizing: border-box;
+    @media ${brakpoints.mobile} { 
+        width: 96vw;
+    }
+    @media ${brakpoints.tablet} { 
+        width: 80vw;
+    }
+    @media ${brakpoints.desktop} { 
+        width: 50vw;
+    }
+    @media ${brakpoints.desktopL} { 
+        width: 40vw;
+    }
+`
+const StyledText = styled.input`
+    background-color: ${props => props.selected ? '#c7b4b4' : '#cecece'}; 
+    border-style: none none solid none;
+    border-color: #EE6C4D;
+    border-width: 2px;
+    font-size: 1.2em;
+    color:#293241;
+    font-family: 'Roboto', sans-serif;
+    flex-grow: 1;
+    padding: 0.5em 0.6em 0.5em 0.6em;
+    &:focus {
+        outline: none;
+    }
+`
+const StyledCopyButton = styled.button`
+    font-family: 'Roboto', sans-serif;
+    background-color: #EE6C4D;
+    border-style: none;
+    font-size: 1em;
+    font-weight: bold;
+    padding: 0.4em 0.5em 0.4em 0.5em;
+    &:hover {
+        cursor: pointer;
+        background-color: #e96240;
+    }
+    &:focus {
+        outline: none;
+    }
+`
+
+function CopyableText({ children }) {
+    const textRef = useRef();
+    const [mouseDown, setMouseDown] = useState(false);
+    const [textCopied, setTextCopied] = useState(false);
+    const [alertFlag, setAlertFlag] = useState(0);
+
+    function copyText() {
+        selectText();
+        navigator.clipboard.writeText(children);
+        setTextCopied(true);
+        setAlertFlag(alertFlag + 1)
+    }
+
+    function selectText() {
+        textRef.current.select();
+        textRef.current.setSelectionRange(0, 99999);
+    }
+
+    return (
+        <StyledContainer>
+            <StyledText
+                type="text"
+                ref={textRef}
+                value={children}
+                onChange={() => { }}
+                onClick={selectText}
+                selected={mouseDown}
+            />
+            <StyledCopyButton
+                onClick={copyText}
+                onMouseDown={() => setMouseDown(true)}
+                onMouseUp={() => setMouseDown(false)}
+            >
+                COPIAR
+            </StyledCopyButton>
+            { textCopied ?
+                <Alert
+                    type="successful"
+                    title={'Enlace copiado al portapapeles'}
+                    restartFlag={alertFlag}
+                /> : ''
+            }
+
+        </StyledContainer>
+    )
+}
+
+export default CopyableText;
